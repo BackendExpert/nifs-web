fb03dcd0507a18b82bb678b48774a9b9314189d6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 11:46:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NewsEvent/NewsEvent.tsx
+++ src/NewsEvent/NewsEvent.tsx
@@ -1,16 +1,35 @@
 
 const NewsEvent = () => {
   return (
-    <div className="container mx-auto px-16">
+    <div className="container mx-auto px-16 mb-20">
         <p className="pt-16 pb-8 text-center text-3xl font-semibold">Recent stories from NIFS</p>
         <div className="md:grid grid-cols-2 gap-4">
             <div className="">
-                <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-11/12 rounded"/>
+                <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="md:w-11/12 rounded"/>
                 <p className="pt-2 text-2xl font-semibold">Award and Recognition</p>
                 <p className="">Three NIFS Researchers Ranked Among Top 2% Scientist in the World Elsevler/ Stanford Citation Analysis</p>
             </div>
             <div className="">
-                
+                <div className="md:grid grid-cols-2 gap-2">
+                    <div className="">
+                        <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="md:w-10/12 rounded"/>
+                    </div>
+                    <div className="md:ml-[-30px]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio adipisci laborum numquam velit beatae quidem iste laudantium necessitatibus ipsam vitae, nisi repudiandae nostrum architecto dolore sint 
+                    </div>
+                    <div className="">
+                        <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-10/12 rounded"/>
+                    </div>
+                    <div className="md:ml-[-30px]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio adipisci laborum numquam velit beatae quidem iste laudantium necessitatibus ipsam vitae, nisi repudiandae nostrum architecto dolore sint 
+                    </div>
+                    <div className="">
+                        <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-10/12 rounded"/>
+                    </div>
+                    <div className="md:ml-[-30px]">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio adipisci laborum numquam velit beatae quidem iste laudantium necessitatibus ipsam vitae, nisi repudiandae nostrum architecto dolore sint 
+                    </div>
+                </div>
             </div>
         </div>
     </div>
