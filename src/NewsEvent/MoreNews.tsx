054315808b520a1da4dbdd797f4531f1d8a9a2fa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 12:36:18 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NewsEvent/MoreNews.tsx
+++ src/NewsEvent/MoreNews.tsx
@@ -10,18 +10,18 @@ const MoreNews = () => {
             <p className="text-4xl font-semibold text-center my-12">News and Events</p>
 
             <div className="md:grid grid-cols-3 gap-2 mb-16">
-                <div className="">
-                    <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" />
+                <div className="md:mb-0 mb-8">
+                    <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="md:w-3/4 h-auto"/>
                     <p className="font-semibold text-xl mt-[-50px] text-white ml-8">Award and Recognition</p>
                     <p className="mt-8">Three NIFS Researchers Ranked Among Top 2% Scientist in the World Elsevler/ Stanford Citation Analysis</p>
                 </div>
-                <div className="">
-                    <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-3/4 h-auto"/>
+                <div className="md:mb-0 mb-8">
+                    <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="md:w-3/4 h-auto"/>
                     <p className="font-semibold text-xl mt-[-50px] text-white ml-8">Award and Recognition</p>
                     <p className="mt-8">Three NIFS Researchers Ranked Among Top 2% Scientist in the World Elsevler/ Stanford Citation Analysis</p>
                 </div>
-                <div className="">
-                    <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-3/4 h-auto"/>
+                <div className="md:mb-0 mb-8">
+                    <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="md:w-3/4 h-auto"/>
                     <p className="font-semibold text-xl mt-[-50px] text-white ml-8">Award and Recognition</p>
                     <p className="mt-8">Three NIFS Researchers Ranked Among Top 2% Scientist in the World Elsevler/ Stanford Citation Analysis</p>
                 </div>
