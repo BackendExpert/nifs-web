1492dd4aac8d1b8291af45997469caec2e5658ae
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 11:33:17 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NewsEvent/NewsEvent.tsx
+++ src/NewsEvent/NewsEvent.tsx
@@ -1,7 +1,12 @@
 
 const NewsEvent = () => {
   return (
-    <div>NewsEvent</div>
+    <div className="container mx-auto px-16">
+        <p className="py-8 text-center text-3xl font-semibold">Recent stories from NIFS</p>
+        <div className="md:grid grid-cols-2 gap-4">
+            
+        </div>
+    </div>
   )
 }
 
