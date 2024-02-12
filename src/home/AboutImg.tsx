e33f8b2de1a3ea2fcddc7c75114f91c82bccfa00
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:32:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -3,7 +3,7 @@ const AboutImg = () => {
   return (
     <div className="container mx-auto px-12">
         <div className="md:grid grid-cols-2 gap-2">
-            <div className="bg-[url()]"></div>
+            <div className="bg-[url(https://wallpapercave.com/wp/wp6599686.jpg)]"></div>
             <div className=""></div>
         </div>
     </div>
