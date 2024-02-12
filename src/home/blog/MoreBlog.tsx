9b8ebbb727c4547a90b9edf2011e5e34edd79b00
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 13:36:59 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/blog/MoreBlog.tsx
+++ src/home/blog/MoreBlog.tsx
@@ -7,8 +7,8 @@ const MoreBlog = () => {
   return (
     <div className="">
         <NavBar />
-        <div className="container mx-auto px-12">
-
+        <div className="container mx-auto px-12 my-16">
+            <p className="text-3xl font-semibold text-center">Recent Scientific Publication</p>
         </div>
         <Footer />
     </div>
