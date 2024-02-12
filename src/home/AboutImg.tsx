d3a69fcc2b65f85ef3249cd2a5d294128513182a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 11:04:04 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -11,10 +11,12 @@ const AboutImg = () => {
         </div>
         <div className="md:mt-[-400px] md:ml-[50%] md:px-0 px-10 md:pt-0 mt-[-270px] md:mb-0 mb-40 md:text-black text-white">
             <p className="mt-20 text-2xl font-semibold">We are the premier institute for Advancing fundamental Sciences</p>
-            <div className="flex text-2xl pt-8 text-blue-400">
-                <p className="pt-[3px] pr-2"><IonIcon name="arrow-forward-circle-outline" ></IonIcon></p>
-                <div className="">Learn more about NIFS</div>
-            </div>
+            <Link to={'/AboutUs'}>
+                <div className="flex text-2xl pt-8 text-blue-400">
+                    <p className="pt-[3px] pr-2"><IonIcon name="arrow-forward-circle-outline" ></IonIcon></p>
+                    <div className="">Learn more about NIFS</div>
+                </div>
+            </Link>
         </div>
         <div className="md:mb-[200px] mb-[-100px]"></div>
     </div>
