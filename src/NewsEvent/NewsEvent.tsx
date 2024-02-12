62f37c175ed2a02f47f42023b88898ee94b7497b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 11:37:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NewsEvent/NewsEvent.tsx
+++ src/NewsEvent/NewsEvent.tsx
@@ -4,7 +4,13 @@ const NewsEvent = () => {
     <div className="container mx-auto px-16">
         <p className="py-8 text-center text-3xl font-semibold">Recent stories from NIFS</p>
         <div className="md:grid grid-cols-2 gap-4">
-            
+            <div className="">
+                <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-11/12 rounded"/>
+                <p className="">Award and Recognition</p>
+            </div>
+            <div className="">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, laborum sint enim suscipit culpa ex laboriosam voluptates unde officia distinctio reiciendis saepe molestias consequatur quod consectetur, natus reprehenderit sequi. Possimus.
+            </div>
         </div>
     </div>
   )
