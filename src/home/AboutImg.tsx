0411d2d1abe2d26652b42c8a2410cfddf117970a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:55:52 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -12,7 +12,7 @@ const AboutImg = () => {
         <div className="md:mt-[-400px] md:ml-[50%] md:px-0 px-10 md:pt-0 mt-[-250px] md:mb-0 mb-40 md:text-black text-white">
             <p className="mt-20 text-2xl font-semibold">We are the premier institute for Advancing fundamental Sciences</p>
             <div className="">
-                <p className=""></p>
+                <p className=""><IonIcon name="arrow-forward-circle-outline" ></IonIcon></p>
             </div>
         </div>
         <div className="md:mb-[300px]"></div>
