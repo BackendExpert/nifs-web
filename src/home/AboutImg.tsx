aa57abe0478d71fdadbda857a69450f51e13839d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:32:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -1,7 +1,12 @@
 
 const AboutImg = () => {
   return (
-    <div className="container mx-auto px-12">AboutImg</div>
+    <div className="container mx-auto px-12">
+        <div className="md:grid grid-cols-2 gap-2">
+            <div className="bg-[url()]"></div>
+            <div className=""></div>
+        </div>
+    </div>
   )
 }
 
