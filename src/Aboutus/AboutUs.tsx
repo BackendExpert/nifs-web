5aa7e8d812282edec26368690cad8601af7c1eb3
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 17:56:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Aboutus/AboutUs.tsx
+++ src/Aboutus/AboutUs.tsx
@@ -7,7 +7,7 @@ const AboutUs = () => {
     <div className="">
         <NavBar />
         <div className="md:grid grid-cols-2 gap-2">
-          <div className="bg-[url(https://wallpapercave.com/wp/wp2468650.jpg)] w-full h-96 bg-cover bg-center">
+          <div className="bg-[url(https://wallpapercave.com/wp/wp2468650.jpg)] w-full h-[550px] bg-cover bg-center">
 
           </div>
           <div className="">
