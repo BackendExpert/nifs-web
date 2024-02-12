a3e5c5b85cc31178b3ec19ce0ca63cedcdb7b51a
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 09:18:03 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,11 +1,14 @@
 import Footer from "../footer/Footer"
 import NavBar from "../nav/Navbar"
+import Countdiv from "./HomeData/CountDiv"
+
 
 const Home = () => {
   return (
     <div>
         <NavBar />
         <img src="https://wallpapercave.com/wp/wp2269706.jpg" alt="" className="h-1/2 w-full"/>
+        <Countdiv />
         <Footer />
     </div>
   )
