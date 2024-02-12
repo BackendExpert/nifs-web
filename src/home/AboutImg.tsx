6e83bbb67203bbf5bdbe6d3a3ca82d34f3df9047
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:54:39 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -5,11 +5,11 @@ const AboutImg = () => {
   return (
     <div className="container mx-auto px-12">
         <div className="md:grid grid-cols-2 gap-2">
-            <div className="rounded bg-[url(https://wallpapercave.com/wp/wp6599686.jpg)] md:ml-16 md:w-3/4 h-[500px] bg-cover bg-center"></div>
+            <div className="rounded bg-[url(https://wallpapercave.com/wp/wp3611699.jpg)] md:ml-16 md:w-3/4 h-[500px] bg-cover bg-center"></div>
             <div className="md:visible invisible">
             </div>
         </div>
-        <div className="md:mt-[-400px] md:ml-[50%]">
+        <div className="md:mt-[-400px] md:ml-[50%] md:px-0 px-10 md:pt-0 mt-[-250px] md:mb-0 mb-40 md:text-black text-white">
             <p className="mt-20 text-2xl font-semibold">We are the premier institute for Advancing fundamental Sciences</p>
             <div className="">
                 <p className=""></p>
