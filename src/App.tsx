5a69cb151d3eac595a928e8ce97bcc73c6ea2c9e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:09:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
 import Contact from "./contact/Contact";
+import ResearchSub from "./research/researchSub";
 
+researchSub
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} ></Route>        
-        <Route path="/Contact" element={<Contact />} ></Route>      
+        <Route path="/Contact" element={<Contact />} ></Route> 
+        <Route path="/Research" element={<ResearchSub />} ></Route>   
       </Routes>
     </BrowserRouter>
   )
