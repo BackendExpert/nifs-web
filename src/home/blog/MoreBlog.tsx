1d0a97d466a81247b4a81b33536c15c2aa078549
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 13:45:33 +0530

    Udpate
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/blog/MoreBlog.tsx
+++ src/home/blog/MoreBlog.tsx
@@ -10,9 +10,21 @@ const MoreBlog = () => {
         <div className="container mx-auto px-12 my-16">
             <p className="text-3xl font-semibold text-center">Recent Scientific Publication</p>
             
-            <div className="md:grid grid-cols-3 gap-2">
+            <div className="md:grid grid-cols-3 gap-2 mt-12">
                 <div className="">
-                    
+                    <img src="https://wallpapercave.com/wp/wp3739590.jpg" alt="" className="w-11/12 h-full rounded"/>
+                    <p className="">Awareness</p>
+                    <p className="">Stop the next pandemic? Stop illegal Wildlife tradel</p>
+                </div>
+                <div className="">
+                    <img src="https://wallpapercave.com/wp/wp13371563.jpg" alt="" className="w-11/12 h-full rounded"/>
+                    <p className="">Awareness</p>
+                    <p className="">The Science of Aji-no-moto</p>
+                </div>
+                <div className="">
+                    <img src="https://wallpapercave.com/wp/wp9102976.jpg" alt="" className="w-11/12 h-full rounded"/>
+                    <p className="">Awareness</p>
+                    <p className="">The Present Covid-19</p>
                 </div>
             </div>
             
