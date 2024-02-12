ef2a7fc6f3789c89d9d34cf800993ff00d89d98c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 11:42:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NewsEvent/NewsEvent.tsx
+++ src/NewsEvent/NewsEvent.tsx
@@ -2,7 +2,7 @@
 const NewsEvent = () => {
   return (
     <div className="container mx-auto px-16">
-        <p className="py-8 text-center text-3xl font-semibold">Recent stories from NIFS</p>
+        <p className="pt-16 pb-8 text-center text-3xl font-semibold">Recent stories from NIFS</p>
         <div className="md:grid grid-cols-2 gap-4">
             <div className="">
                 <img src="https://wallpapercave.com/wp/wp11190895.jpg" alt="" className="w-11/12 rounded"/>
@@ -10,7 +10,7 @@ const NewsEvent = () => {
                 <p className="">Three NIFS Researchers Ranked Among Top 2% Scientist in the World Elsevler/ Stanford Citation Analysis</p>
             </div>
             <div className="">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum, laborum sint enim suscipit culpa ex laboriosam voluptates unde officia distinctio reiciendis saepe molestias consequatur quod consectetur, natus reprehenderit sequi. Possimus.
+                
             </div>
         </div>
     </div>
