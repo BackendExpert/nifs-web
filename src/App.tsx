dcd045d74b4a320f4f6fd2d21a32caac28d50ffd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 09:21:42 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./home/Home";
+import Contact from "./contact/Contact";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} ></Route>        
+        <Route path="/Contact" element={<Contact />} ></Route>      
       </Routes>
     </BrowserRouter>
   )
