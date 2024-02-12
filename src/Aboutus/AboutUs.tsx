df1522da8f07c584de38532685a527edcc54112f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 17:52:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Aboutus/AboutUs.tsx
+++ src/Aboutus/AboutUs.tsx
@@ -2,7 +2,14 @@
 const AboutUs = () => {
   return (
     <div className="container mx-auto px-12">
-      
+      <div className="md:grid grid-cols-2 gap-2">
+        <div className="">
+
+        </div>
+        <div className="">
+          
+        </div>
+      </div>
     </div>
   )
 }
