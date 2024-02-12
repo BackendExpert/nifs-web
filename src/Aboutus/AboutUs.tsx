9c72b5acefad7a08b236a00cddd12b2e00bb2909
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 18:08:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Aboutus/AboutUs.tsx
+++ src/Aboutus/AboutUs.tsx
@@ -10,13 +10,14 @@ const AboutUs = () => {
           <div className="bg-[url(https://wallpapercave.com/wp/wp2468650.jpg)] w-full h-[550px] bg-cover bg-center">
           </div>
           <div className="bg-gray-700">
+            <div className="">
+              <p className="text-4xl px-8 text-yellow-500 pt-40">We are the premier institute for advancing fundamental Sciences</p>
+              <p className="text-white px-8">NIFS is engaged in scientific research to facilitate fundamental and advanced studies with an emphasis on basic research for national development as well as for the advancement of Science</p>
+            </div>
           </div>
         </div>
-        <div className="md:mt-[-350px] md:ml-[52%]">
-          <p className="text-4xl pr-4 text-yellow-500 pb-2">We are the premier institute for advancing fundamental Sciences</p>
-          <p className="text-white pr-4">NIFS is engaged in scientific research to facilitate fundamental and advanced studies with an emphasis on basic research for national development as well as for the advancement of Science</p>
-        </div>
-        <div className="md:mb-80"></div>
+
+
         <Footer />
     </div>
   )
