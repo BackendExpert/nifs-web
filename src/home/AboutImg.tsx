230b8e7f0c735a24295c5cefcaa88ad53c45cffc
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:41:23 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -6,6 +6,9 @@ const AboutImg = () => {
     <div className="container mx-auto px-12">
         <div className="md:grid grid-cols-2 gap-2">
             <div className="bg-[url(https://wallpapercave.com/wp/wp6599686.jpg)] ml-16 w-3/4 h-[500px] bg-cover bg-center"></div>
+            <div className="md:s">
+            </div>
+
             <div className="">
                 <p className="mt-20 text-2xl font-semibold">We are the premier institute for Advancing fundamental Sciences</p>
                 <div className="">
