a53061d3200f1ddbd0ac6bb4907b87a0962dec2f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:13:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./home/Home";
 import Contact from "./contact/Contact";
 import ResearchSub from "./research/researchSub";
 import BIo from "./research/BIo";
+import Evolution from "./research/Bio/Evolution";
 
 
 export default function App() {
@@ -13,6 +14,7 @@ export default function App() {
         <Route path="/Contact" element={<Contact />} ></Route> 
         <Route path="/Research" element={<ResearchSub />} ></Route> 
         <Route path="/Research/Bio" element={<BIo />} ></Route>
+        <Route path="/Research/Bio/Evolution" element={<Evolution />} ></Route>
       </Routes>
     </BrowserRouter>
   )
