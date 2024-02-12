00ee0d1dac9b214786e8e17009a08413c275a27e
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 13:48:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/blog/MoreBlog.tsx
+++ src/home/blog/MoreBlog.tsx
@@ -11,17 +11,32 @@ const MoreBlog = () => {
             <p className="text-3xl font-semibold text-center">Recent Scientific Publication</p>
             
             <div className="md:grid grid-cols-3 gap-2 mt-12">
-                <div className="">
+                <div className="mb-16">
                     <img src="https://wallpapercave.com/wp/wp3739590.jpg" alt="" className="w-11/12 h-full rounded"/>
                     <p className="mt-[-35px] text-xl font-semibold text-white ml-2">Awareness</p>
                     <p className="mt-4">Stop the next pandemic? Stop illegal Wildlife tradel</p>
                 </div>
-                <div className="">
+                <div className="mb-16">
                     <img src="https://wallpapercave.com/wp/wp13371563.jpg" alt="" className="w-11/12 h-full rounded"/>
                     <p className="mt-[-35px] text-xl font-semibold text-white ml-2">Awareness</p>
                     <p className="mt-4">The Science of Aji-no-moto</p>
                 </div>
-                <div className="">
+                <div className="mb-16">
+                    <img src="https://wallpapercave.com/wp/wp9102976.jpg" alt="" className="w-11/12 h-full rounded"/>
+                    <p className="mt-[-35px] text-xl font-semibold text-white ml-2">Awareness</p>
+                    <p className="mt-4">The Present Covid-19</p>
+                </div>
+                <div className="mb-16">
+                    <img src="https://wallpapercave.com/wp/wp3739590.jpg" alt="" className="w-11/12 h-full rounded"/>
+                    <p className="mt-[-35px] text-xl font-semibold text-white ml-2">Awareness</p>
+                    <p className="mt-4">Stop the next pandemic? Stop illegal Wildlife tradel</p>
+                </div>
+                <div className="mb-16">
+                    <img src="https://wallpapercave.com/wp/wp13371563.jpg" alt="" className="w-11/12 h-full rounded"/>
+                    <p className="mt-[-35px] text-xl font-semibold text-white ml-2">Awareness</p>
+                    <p className="mt-4">The Science of Aji-no-moto</p>
+                </div>
+                <div className="mb-16">
                     <img src="https://wallpapercave.com/wp/wp9102976.jpg" alt="" className="w-11/12 h-full rounded"/>
                     <p className="mt-[-35px] text-xl font-semibold text-white ml-2">Awareness</p>
                     <p className="mt-4">The Present Covid-19</p>
