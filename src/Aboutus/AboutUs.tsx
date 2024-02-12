88fd4e0443a5420899646444102788f361a9da86
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 18:09:48 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/Aboutus/AboutUs.tsx
+++ src/Aboutus/AboutUs.tsx
@@ -11,8 +11,8 @@ const AboutUs = () => {
           </div>
           <div className="bg-gray-700">
             <div className="">
-              <p className="text-4xl px-8 text-yellow-500 pt-40">We are the premier institute for advancing fundamental Sciences</p>
-              <p className="text-white px-8">NIFS is engaged in scientific research to facilitate fundamental and advanced studies with an emphasis on basic research for national development as well as for the advancement of Science</p>
+              <p className="md:text-4xl text-3xl px-8 text-yellow-500 md:pt-40 pt-16">We are the premier institute for advancing fundamental Sciences</p>
+              <p className="text-white px-8 md:pb-0 pb-16">NIFS is engaged in scientific research to facilitate fundamental and advanced studies with an emphasis on basic research for national development as well as for the advancement of Science</p>
             </div>
           </div>
         </div>
