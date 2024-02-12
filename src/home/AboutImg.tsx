650e557c9fc9e958081fcc72f23e9b63c2c093c7
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-12 10:38:25 +0530

    Udpaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/home/AboutImg.tsx
+++ src/home/AboutImg.tsx
@@ -1,3 +1,5 @@
+import IonIcon from "@reacticons/ionicons"
+
 
 const AboutImg = () => {
   return (
@@ -5,7 +7,10 @@ const AboutImg = () => {
         <div className="md:grid grid-cols-2 gap-2">
             <div className="bg-[url(https://wallpapercave.com/wp/wp6599686.jpg)] ml-16 w-3/4 h-[500px] bg-cover bg-center"></div>
             <div className="">
-                <p className="">We are the premier institute for Advancing fundamental Sciences</p>
+                <p className="mt-20 text-2xl font-semibold">We are the premier institute for Advancing fundamental Sciences</p>
+                <div className="">
+                    <p className=""></p>
+                </div>
             </div>
         </div>
     </div>
